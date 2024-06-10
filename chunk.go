780ccc62e74a2235c4ed70fe877c0a6b26926fab
thatmattlove <matt@stunninglyclear.com>
2024-06-10 15:45:18 -0400

    fix `ChunkSlice`

--- chunk.go
+++ chunk.go
@@ -1,10 +1,16 @@
 package utils
 
-// ChunkSlice creates a slice of slices of max size.
+// ChuckSlice creates a slice of slices of a max chunk size.
+// Given a size argument of 4:
+//
+//	[]int{1,2,3,4,5,6,7,9}
+//	// Becomes:
+//	[][]int{ {1,2,3,4}, {5,6,7,8}, {9} }
 func ChunkSlice[T any](items []T, size int) [][]T {
-	chunks := make([][]T, 0, (len(items)/size)+1)
-	for size > len(items) {
-		items, chunks = items[size:], append(chunks, items[0:size:size])
+	batches := make([][]T, 0, (len(items)+size-1)/size)
+	for size < len(items) {
+		items, batches = items[size:], append(batches, items[0:size:size])
 	}
-	return chunks
+	batches = append(batches, items)
+	return batches
 }
--- chunk_test.go
+++ chunk_test.go
@@ -14,18 +14,21 @@ func Test_ChunkSlice(t *testing.T) {
 		out := utils.ChunkSlice(in, 2)
 		assert.Len(t, out, 3)
 	})
+	t.Run("uneven", func(t *testing.T) {
+		t.Parallel()
+		in := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9"}
+		out := utils.ChunkSlice(in, 4)
+		assert.Len(t, out, 3)
+		assert.Equal(t, [][]string{{"1", "2", "3", "4"}, {"5", "6", "7", "8"}, {"9"}}, out)
+	})
 	t.Run("struct", func(t *testing.T) {
 		t.Parallel()
 		type S struct {
 			One int
 			Two int
 		}
-		in := []S{{1, 2}, {3, 4}, {5, 6}}
+		in := []S{{1, 2}, {3, 4}, {5, 6}, {7, 8}, {9, 10}, {11, 12}}
 		out := utils.ChunkSlice(in, 2)
-		assert.Len(t, out, 2)
-		assert.Equal(t, 1, out[0][0].One)
-		assert.Equal(t, 3, out[0][1].One)
-		assert.Equal(t, 5, out[1][0].One)
+		assert.Len(t, out, 3)
 	})
-
 }
